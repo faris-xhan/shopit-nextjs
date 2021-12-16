7927c7af97e32a8f06740c34981e2c715000a648
faris <faris@mail.com>
2021-12-16 17:44:28 +0500

    :art: setup bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <Head>
+        <title>Shopit - Buy what you love.</title>
+      </Head>
+      <Component {...pageProps} />;
+    </SSRProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
