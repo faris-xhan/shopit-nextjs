c081aa983ce8adb7d53b2ca14a9f3b5dfad8ba49
faris <faris@mail.com>
2021-12-16 18:39:05 +0500

    :sparkles: add buttons on header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,31 +1,46 @@
+import Link from 'next/link';
+import Nav from 'react-bootstrap/Nav';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import styles from './header.module.css';
+import { Cart, Person, Search } from 'react-bootstrap-icons';
 
 export const Header = (props) => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav>
+            <Link href="/" passHref>
+              <Nav.Link>Home</Nav.Link>
+            </Link>
+            <NavDropdown title="Products" id="products-dropdown">
+              <Link href="products/shopping-bag" passHref>
+                <NavDropdown.Item>Shopping Bag</NavDropdown.Item>
+              </Link>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        <div className="flex-grow-1 px-4">
+          <Link href="/" passHref>
+            <Navbar.Brand>Shopit</Navbar.Brand>
+          </Link>
+        </div>
+        <ButtonGroup>
+          <Button variant="none" className={styles.iconButton}>
+            <Search fontSize={20} />
+          </Button>
+          <Button variant="none" className={styles.iconButton}>
+            <Person fontSize={20} />
+          </Button>
+          <Button variant="none" className={styles.iconButton}>
+            <Cart fontSize={20} />
+          </Button>
+        </ButtonGroup>
       </Container>
     </Navbar>
   );
